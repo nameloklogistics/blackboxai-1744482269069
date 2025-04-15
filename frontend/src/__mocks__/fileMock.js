940363b3fb80675bf97122536b9ba608b371d74a
nameloklogistics <nameloklogistics@gmail.com>
2025-04-15 19:06:43 +0300

    Add frontend/src/__mocks__/fileMock.js 1744733202712

---
+++ frontend/src/__mocks__/fileMock.js
@@ -0,0 +1,8 @@
+// This file mocks file imports for Jest
+// When a module imports a file (like an image or CSS file), Jest will use this mock instead
+
+module.exports = {
+  __esModule: true,
+  default: 'test-file-stub',
+  ReactComponent: () => 'test-file-stub',
+};
